1f400b650fbaf0ea9887f338ee1226df86b0e47a
nastiglugan <54989161+nastiglugan@users.noreply.github.com>
2024-12-11 18:34:28 +0000

    Deploying to gh-pages from @ nastiglugan/goit-js-hw-09_restart@730b32695ee72b871623de16c44991a7b1f98d43 🚀

---
+++ 01-color-switcher.2324db9a.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(){o=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),t.setAttribute("disabled","")})),e.addEventListener("click",(function(){t.removeAttribute("disabled",""),clearInterval(o)}));var o=null}();
+//# sourceMappingURL=01-color-switcher.2324db9a.js.map
---
+++ 01-color-switcher.325bb687.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");t.addEventListener("click",(function(){r=setInterval((()=>{o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.setAttribute("disabled","")})),e.addEventListener("click",(function(){t.removeAttribute("disabled",""),clearInterval(r)}));let r=null;
+//# sourceMappingURL=01-color-switcher.325bb687.js.map
