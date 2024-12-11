730b32695ee72b871623de16c44991a7b1f98d43
NastiHluhan <0506167197more@gmail.com>
2024-12-11 20:34:05 +0200

    color

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
 
+startBtn.addEventListener('click', onStart);
+stopBtn.addEventListener('click', onStop);
+let timerId = null;
+
+function onStart() {
+  timerId = setInterval(() => {
+    changeColor();
+  }, 1000);
+  startBtn.setAttribute('disabled', '');
+}
+
+function onStop() {
+  startBtn.removeAttribute('disabled', '');
+  clearInterval(timerId);
+}
+
+function changeColor() {
+  body.style.backgroundColor = getRandomHexColor();
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
